e3967ce9ddeaa3ef3b48ae7a3f26acf9471cff00
GB <gb@owner>
2012-10-27 01:42:15 -0400

    removed no longer needed generic put route (replaced with subverbs)

--- node/routes/conf.js
+++ node/routes/conf.js
@@ -3,8 +3,8 @@ var confHandler = require("../handlers/confHandler");
 module.exports = function(apiServer){
     apiServer.get('/conf', confHandler.list);
     apiServer.post('/conf', confHandler.create);
+
     apiServer.get('/conf/:friendlyName', confHandler.single);
-    apiServer.put('/conf/:friendlyName', confHandler.modify);
     apiServer.del('/conf/:friendlyName', confHandler.remove);
 
     apiServer.put('/conf/:friendlyName/property', confHandler.addProperty);
